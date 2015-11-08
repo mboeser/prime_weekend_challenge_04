9bef6481be577690861438799d209cd4cbbe3dc4
Mike Boeser <mboeser@yahoo.com>
2015-11-08 12:52:20 -0600

    working local test

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,7 @@ var path = require('path');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://mboeser@yahoo.com:nUF-Je3-rKn-neB@ds051524.mongolab.com:51524/heroku_bkcrmf82');
+mongoose.connect('mongodb://mike:test1234@ds051534.mongolab.com:51534/testwknd04');
 mongoose.model('Message', new Schema({"name": String, "message": String}, {collection: 'messages'}));
 var Message = mongoose.model('Message');
 
